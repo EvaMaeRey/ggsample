edacf2674e9d9fa58939caaaf2fa39728200541f
EvaMaeRey <evangeline.mae@gmail.com>
2021-09-15 00:07:53 -0400

    adding facet functions

--- R/facet_sample.R
+++ R/facet_sample.R
@@ -30,50 +30,50 @@ facet_sample <- function(n = 16, n_sampled = 5, nrow = NULL, ncol = NULL,
                          seed = sample(2000:3000, 1)) {
 
   facet <- ggplot2::facet_wrap(~.bootstrap, nrow = nrow, ncol = ncol, scales = scales,
-                      shrink = shrink, strip.position = strip.position)
+                               shrink = shrink, strip.position = strip.position)
   facet$params$n <- n
   facet$params$n_sampled <- n_sampled
   facet$params$seed <- seed
 
   ggplot2::ggproto(NULL, FacetSample,
-          shrink = shrink,
-          params = facet$params
+                   shrink = shrink,
+                   params = facet$params
   )
 }
 
 FacetSample <- ggplot2::ggproto("FacetSample", ggplot2::FacetWrap,
-                       compute_layout = function(data, params) {
-                         id <- seq_len(params$n)
+                                compute_layout = function(data, params) {
+                                  id <- seq_len(params$n)
 
-                         dims <- wrap_dims(params$n, params$nrow, params$ncol)
-                         layout <- data.frame(PANEL = factor(id))
+                                  dims <- wrap_dims(params$n, params$nrow, params$ncol)
+                                  layout <- data.frame(PANEL = factor(id))
 
-                         if (params$as.table) {
-                           layout$ROW <- as.integer((id - 1L) %/% dims[2] + 1L)
-                         } else {
-                           layout$ROW <- as.integer(dims[1] - (id - 1L) %/% dims[2])
-                         }
-                         layout$COL <- as.integer((id - 1L) %% dims[2] + 1L)
+                                  if (params$as.table) {
+                                    layout$ROW <- as.integer((id - 1L) %/% dims[2] + 1L)
+                                  } else {
+                                    layout$ROW <- as.integer(dims[1] - (id - 1L) %/% dims[2])
+                                  }
+                                  layout$COL <- as.integer((id - 1L) %% dims[2] + 1L)
 
-                         layout <- layout[order(layout$PANEL), , drop = FALSE]
-                         rownames(layout) <- NULL
+                                  layout <- layout[order(layout$PANEL), , drop = FALSE]
+                                  rownames(layout) <- NULL
 
-                         # Add scale identification
-                         layout$SCALE_X <- if (params$free$x) id else 1L
-                         layout$SCALE_Y <- if (params$free$y) id else 1L
+                                  # Add scale identification
+                                  layout$SCALE_X <- if (params$free$x) id else 1L
+                                  layout$SCALE_Y <- if (params$free$y) id else 1L
 
-                         cbind(layout, .bootstrap = id)
-                       },
-                       map_data = function(data, layout, params) {
-                         if (is.null(data) || nrow(data) == 0) {
-                           return(cbind(data, PANEL = integer(0)))
-                         }
-                         set.seed(params$seed)
-                         # n_samples <- round(nrow(data) * params$prop)
-                         n_sampled <- params$n_sampled
-                         new_data <- lapply(seq_len(params$n), function(i) {
-                           cbind(data[sample(nrow(data), n_sampled), , drop = FALSE], PANEL = i)
-                         })
-                         do.call(rbind, new_data)
-                       }
+                                  cbind(layout, .bootstrap = id)
+                                },
+                                map_data = function(data, layout, params) {
+                                  if (is.null(data) || nrow(data) == 0) {
+                                    return(cbind(data, PANEL = integer(0)))
+                                  }
+                                  set.seed(params$seed)
+                                  # n_samples <- round(nrow(data) * params$prop)
+                                  n_sampled <- params$n_sampled
+                                  new_data <- lapply(seq_len(params$n), function(i) {
+                                    cbind(data[sample(nrow(data), n_sampled), , drop = FALSE], PANEL = i)
+                                  })
+                                  do.call(rbind, new_data)
+                                }
 )
